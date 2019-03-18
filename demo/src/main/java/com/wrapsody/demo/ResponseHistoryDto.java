6ac759ed3d2b6961a2a1642b4548a82b8d08cd01
jinbeomdev <niutn@naver.com>
2019-03-18 17:02:17 +0900

    change dto

--- demo/src/main/java/com/wrapsody/demo/ResponseHistoryDto.java
+++ demo/src/main/java/com/wrapsody/demo/ResponseHistoryDto.java
@@ -9,8 +9,19 @@ import java.util.List;
 @AllArgsConstructor
 @Getter
 @Setter
-public class ResponseHistoryDto {
-    private History history;
+public class ResponseHistoryDto extends History {
     private List<HistoryTag> tags;
     private List<HistoryAuth> auths;
+
+    public ResponseHistoryDto(History history, List<HistoryTag> tags, List<HistoryAuth> auths) {
+        setId(history.getId());
+        setHistoryMasterId(history.getHistoryMasterId());
+        setHistoryMasterName(history.getHistoryMasterName());
+        setHistoryPreSetName(history.getHistoryPreSetName());
+        setHistoryViewAuthAllUsers(history.getHistoryViewAuthAllUsers());
+        setHistoryIsDeleted(history.getHistoryIsDeleted());
+
+        this.tags = tags;
+        this.auths = auths;
+    }
 }
