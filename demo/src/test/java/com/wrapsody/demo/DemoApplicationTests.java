87a1ae3b8a2734ff5fba023c0d76e2531858175f
jinbeom <niutn@naver.com>
2019-03-20 16:36:23 +0900

    test

--- demo/src/test/java/com/wrapsody/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/wrapsody/demo/DemoApplicationTests.java
@@ -1,14 +1,9 @@
 package com.wrapsody.demo;
 
 import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.client.HttpClients;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -38,42 +33,5 @@ public class DemoApplicationTests {
                 httpResponse.getStatusLine().getStatusCode(),
                 equalTo(404)
         );
-    }
-
-    @Test
-    public void requestWrapsodyCreateHistoryWithBadMasterId() throws IOException {
-        String syncId = "201902221730014cdeac96d86649f385ba485e972032aa";
-
-        CloseableHttpClient client = HttpClients.createDefault();
-        HttpPost httpPost = new HttpPost("http://localhost:8080/history?syncId=" + syncId);
-
-        String json = "{\n" +
-                "  \"historyMasterId\":\"wrapsodynewuser\",\n" +
-                "  \"historyMasterName\":\"test\",\n" +
-                "  \"historyPreSetName\":\"test\",\n" +
-                "  \"historyViewAuthAllUsers\":\"false\",\n" +
-                "  \"tags\":[\n" +
-                "    {\"historyTagCode\":\"\", \"historyTagName\":\"aaa\", \"historyTagType\":\"REQUIRED\"},\n" +
-                "    {\"historyTagCode\":\"\", \"historyTagName\":\"testtag1\", \"historyTagType\":\"CUSTOM\"},\n" +
-                "    {\"historyTagCode\":\"\", \"historyTagName\":\"testtag2\", \"historyTagType\":\"CUSTOM\"}\n" +
-                "  ],\n" +
-                "  \"auths\":[\n" +
-                "    {\"historyAuthId\":\"lbj2\", \"historyAuthName\":\"test2\", \"historyAuthType\":\"REVISION\"},\n" +
-                "    {\"historyAuthId\":\"syong\", \"historyAuthName\":\"test3\", \"historyAuthType\":\"REVISION\"},\n" +
-                "    {\"historyAuthId\":\"genie\", \"historyAuthName\":\"test4\", \"historyAuthType\":\"REVISION\"},\n" +
-                "    {\"historyAuthId\":\"minji.\", \"historyAuthName\":\"test5\", \"historyAuthType\":\"REVISION\"},\n" +
-                "    {\"historyAuthId\":\"minji..\", \"historyAuthName\":\"test6\", \"historyAuthType\":\"REVISION\"},\n" +
-                "    {\"historyAuthId\":\"jinbeom\", \"historyAuthName\":\"test7\", \"historyAuthType\":\"READ\"}\n" +
-                "  ]\n" +
-                "\n" +
-                "}";
-
-        StringEntity entity = new StringEntity(json);
-        httpPost.setEntity(entity);
-        httpPost.setHeader("Content-type", "application/json");
-
-        CloseableHttpResponse response = client.execute(httpPost);
-        assertThat(response.getStatusLine().getStatusCode(), equalTo(200));
-        client.close();
     }
 }
